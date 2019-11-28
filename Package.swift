7290ba263fafbd6e50ad2a7a30a3f6a38deb90d0
Mats Eikeland Mollestad <mem@mollestad.no>
2019-11-28 21:34:11 +0100

    Changed linux dependencies

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ dependencies.append(contentsOf: [
 )
 #else
 dependencies.append(contentsOf: [
-        .package(url: "https://MatsKognita:dyjdov-bupgev-goffY8@bitbucket.org/MatsEikelandMollestad/kognita-core.git", from: "1.0.0"),
-        .package(url: "https://MatsKognita:dyjdov-bupgev-goffY8@bitbucket.org/MatsEikelandMollestad/kognitapages.git", from: "1.0.0")
+        .package(url: "https://Kognita:dyjdov-bupgev-goffY8@github.com/MatsMoll/KognitaCore", from: "1.0.0"),
+        .package(url: "https://Kognita:dyjdov-bupgev-goffY8@github.com/MatsMoll/KognitaPages", from: "1.0.0")
     ]
 )
 #endif
