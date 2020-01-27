7ba7317d31a7a7a9262765b593597d06f95c4266
Mats Eikeland Mollestad <mem@mollestad.no>
2020-01-27 16:05:32 +0100

    [User.VerifyEmail] Added support for email verification

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -43,84 +43,21 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
         )
     }
 
-    let templates = try setupTemplates()
-    services.register(templates)
-    services.register(templates, as: ResetPasswordMailRenderable.self)
+    let renderer = try KognitaViews.renderer(env: env)
+
+    let path = DirectoryConfig.detect().workDir + "Resources/Localization"
+    try renderer.registerLocalization(atPath: path, defaultLocale: "nb")
+    renderer.timeZone = TimeZone(identifier: "CET") ?? .current
+
+    services.register(renderer)
+    services.register(renderer, as: ResetPasswordMailRenderable.self)
+    services.register(renderer, as: VerifyEmailRenderable.self)
 }
 
 private func registerRouter(in services: inout Services) throws {
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-}
-
-func setupTemplates() throws -> HTMLRenderer {
-
-    let renderer = HTMLRenderer()
-
-    let path = DirectoryConfig.detect().workDir + "Resources/Localization"
-    try renderer.registerLocalization(atPath: path, defaultLocale: "nb")
-
-    renderer.timeZone = TimeZone(identifier: "CET") ?? .current
-
-    // Starter
-    try renderer.add(view: Pages.Landing())
-
-    // Legal
-    try renderer.add(view: Pages.PrivacyPolicy())
-    try renderer.add(view: Pages.TermsOfService())
-
-    // Error Pages
-    try renderer.add(view: Pages.ServerError())
-    try renderer.add(view: Pages.NotFoundError())
-
-    // Auth
-    try renderer.add(view: LoginPage())
-    try renderer.add(view: User.Templates.Signup())
-    try renderer.add(view: User.Templates.ResetPassword.Start())
-    try renderer.add(view: User.Templates.ResetPassword.Mail())
-    try renderer.add(view: User.Templates.ResetPassword.Reset())
-
-    // Main User pages
-    try renderer.add(view: Subject.Templates.ListOverview())
-    try renderer.add(view: Subject.Templates.Details())
-    try renderer.add(view: Subject.Templates.SelectRedirect())
-
-    try renderer.add(view: SubjectTest.Templates.Modify())
-    try renderer.add(view: SubjectTest.Templates.List())
-    try renderer.add(view: SubjectTest.Templates.Monitor())
-    try renderer.add(view: SubjectTest.Templates.StatusSection())
-    try renderer.add(view: SubjectTest.Templates.Results())
-
-    try renderer.add(view: TestSession.Templates.Overview())
-    try renderer.add(view: TestSession.Templates.Results())
-
-//    // Task Overview
-//    try renderer.add(template: TaskOverviewListTemplate())
-
-//    // Task Template
-    try renderer.add(view: FlashCardTask.Templates.Execute())
-    try renderer.add(view: MultipleChoiseTask.Templates.Execute())
-    try renderer.add(view: TaskSolutionsTemplate())
-    try renderer.add(view: MultipleChoiseTaskTestMode())
-//
-//    // Create Content
-    try renderer.add(view: Subject.Templates.Create())
-    try renderer.add(view: Topic.Templates.Create())
-    try renderer.add(view: Subtopic.Templates.Create())
-//
-//    // Create Task Templates
-    try renderer.add(view: FlashCardTask.Templates.Create())
-    try renderer.add(view: MultipleChoiseTask.Templates.Create())
-//
-//    // Practice Session
-    try renderer.add(view: PracticeSession.Templates.History())
-    try renderer.add(view: PracticeSession.Templates.Result())
-
-//    // Creator pages
-    try renderer.add(view: Subject.Templates.ContentOverview())
-//    try renderer.add(template: CreatorInformationPage())
-    return renderer
 }
 
 extension HTMLRenderer: ResetPasswordMailRenderable {
@@ -132,5 +69,17 @@ extension HTMLRenderer: ResetPasswordMailRenderable {
                     changeUrl: "https://uni.kognita.no/reset-password?token=\(token.token)"
                 )
         )
+    }
+}
+
+extension HTMLRenderer: VerifyEmailRenderable {
+    public func render(with content: User.VerifyEmail.EmailContent, on container: Container) throws -> EventLoopFuture<String> {
+        let html = try render(
+            raw: User.Templates.VerifyMail.self,
+            with: User.Templates.VerifyMail.Context(
+                token: content
+            )
+        )
+        return container.future(html)
     }
 }
