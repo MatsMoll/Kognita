877d76267d26d6f848bc6d05b44e731c73041b9b
eskildbr <eskildbr@stud.ntnu.no>
2020-02-27 09:19:37 +0100

    [Fix] Added javascript for discussion so that you can write a discussion question without changing page

---
+++ Public/assets/js/task-discussion/create.js
@@ -0,0 +1,35 @@
+
+function createDiscussion() {
+
+    var url = "/api/task-discussion";
+    
+    let description = $("#create-discussion-question").val();
+    let taskID = parseInt($("#task-id").val());
+
+    var data = JSON.stringify({
+        "description": description,
+        "taskID" : taskID
+    });
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type" : "application/json"
+        },
+        body: data
+    })
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.statusText);
+        }
+    })
+    .catch(function (error) {
+        $("#submitButton").attr("disabled", false);
+        $("#error-massage").text(error.message);
+        $("#error-div").fadeIn();
+        $("#error-div").removeClass("d-none");
+    });
+}
