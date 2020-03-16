d703055fb25b0ef51d49e29d0c86ecd7b794a20c
eskildbr <eskildbr@stud.ntnu.no>
2020-03-16 12:45:50 +0100

    [Fix] Discussion description must contain atleast 4 characters

--- Public/assets/js/task-discussion/create.js
+++ Public/assets/js/task-discussion/create.js
@@ -2,39 +2,54 @@ function refreshPage(){
     window.location.reload();
 } 
 
-function createDiscussion() {
-
-    var url = "/api/task-discussion";
-    
-    let description = $("#create-discussion-question").val();
+function taskDiscussionData() {
+    let description = creatediscussionquestion.value();
     let taskID = parseInt($("#task-id").val());
 
-    var data = JSON.stringify({
+    if(description.length < 4) {
+        throw Error("Lengden av diskusjonsspørsmålet er for lite")
+    }
+
+    return JSON.stringify({
         "description": description,
         "taskID" : taskID
     });
+}
 
-    $("#create-discussion-question").val("");
+function createDiscussion() {
 
-    fetch(url, {
-        method: "POST",
-        headers: {
-            "Accept": "application/json, text/plain, */*",
-            "Content-Type" : "application/json"
-        },
-        body: data
-    })
-    .then(function (response) {
-        if (response.ok) {
-            refreshPage()
-        } else {
-            throw new Error(response.statusText);
-        }
-    })
-    .catch(function (error) {
+    var url = "/api/task-discussion";
+
+    creatediscussionquestion.value("");
+
+    try {
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Accept": "application/json, text/plain, */*",
+                "Content-Type" : "application/json"
+            },
+            body: taskDiscussionData()
+        })
+        .then(function (response) {
+            if (response.ok) {
+                refreshPage()
+                presentControlls()
+                pres
+            } else {
+                throw new Error(response.statusText);
+            }
+        })
+        .catch(function (error) {
+            $("#submitButton").attr("disabled", false);
+            $("#error-massage").text(error.message);
+            $("#error-div").fadeIn();
+            $("#error-div").removeClass("d-none");
+        });
+    } catch (error) {
         $("#submitButton").attr("disabled", false);
         $("#error-massage").text(error.message);
         $("#error-div").fadeIn();
         $("#error-div").removeClass("d-none");
-    });
+    }
 }
