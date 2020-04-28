52f0990faca3d2ec85976686166f9794b45076fa
Mats Eikeland Mollestad <mem@mollestad.no>
2020-04-28 20:50:49 +0200

    [ci] updated swiftlint install

--- CI/install-swiftlint.sh
+++ CI/install-swiftlint.sh
@@ -3,7 +3,6 @@
 git clone https://github.com/realm/SwiftLint.git
 cd SwiftLint
 git checkout 0.39.2
-swift build -c release --static-swift-stdlib
-mv .build/x86_64-unknown-linux/release/swiftlint /usr/local/bin/
+swift build -c release --static-swift-stdlib --build-path=/usr/local/bin/
 cd ..
 rm -rf SwiftLint
