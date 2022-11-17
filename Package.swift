03a198147fd2e1083f48a72dd99be0a1263be749
Mats E. Mollestad <mats@mollestad.no>
2022-11-18 00:05:32 +0100

    fix: package

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ var dependencies: [Package.Dependency] = [
     // 💧 A server-side Swift web framework.
     .package(name: "vapor", url: "https://github.com/vapor/vapor.git", from: "4.67.3"),
 
-    .package(name: "HTMLKitVaporProvider", url: "https://github.com/MatsMoll/htmlkit-vapor-provider.git", from: "1.0.1"),
+    .package(name: "HTMLKitVaporProvider", url: "https://github.com/MatsMoll/htmlkit-vapor-provider.git", .exact("1.0.1")),
     
     .package(url: "https://github.com/vapor-community/HTMLKit.git", .exact("2.1.1")),
     
